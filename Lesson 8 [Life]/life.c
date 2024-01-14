62116a6d39f5c5fd21845200982f5701ef2657f7
ydn-rs <ydn.rs@yandex.ru>
2024-01-14 12:00:47 +0300

    Fix LifeGame

--- Lesson 8 [Life]/life.c
+++ Lesson 8 [Life]/life.c
@@ -10,19 +10,40 @@ void generationBoard(int N, int t) {
     board = (int **) malloc(N * sizeof(int *));
     for (int i = 0; i < N; i++) {
         board[i] = (int *) malloc(N * sizeof(int));
+        for (int j = 0; j < N; j++) {
+            board[i][j] = 0;
+        }
     }
-    for (int i = 0; i < t; i++) {
+
+    int count = 0;
+    while (count < t) {
         int row = rand() % N;
         int col = rand() % N;
-        board[row][col] = 1;
+        if (board[row][col] == 0) {
+            board[row][col] = 1;
+            count++;
+        }
     }
 }
+
 
 void clearBoard(int N) {
     for (int i = 0; i < N; i++) {
         free(board[i]);
     }
     free(board);
+}
+
+int countNeigh(int row, int col, int N) {
+    int liveNeighbors = 0;
+    for (int i = row - 1; i <= row + 1; i++) {
+        for (int j = col - 1; j <= col + 1; j++) {
+            if (i >= 0 && i < N && j >= 0 && j < N && !(i == row && j == col)) {
+                liveNeighbors += board[i][j];
+            }
+        }
+    }
+    return liveNeighbors;
 }
 
 void printBoard(int N) {
@@ -44,23 +65,20 @@ void updateBoard(int N) {
 
     for (int i = 0; i < N; i++) {
         for (int j = 0; j < N; j++) {
-            int liveNeighCells = 0;
-            for (int x = -1; x <= 1; x++) {
-                for (int y = -1; y <= 1; y++) {
-                    int cellsRow = (i + x + N) % N;
-                    int cellsCol = (j + y + N) % N;
-                    liveNeighCells += board[cellsRow][cellsCol];
+            int liveNeighCells = countNeigh(i, j, N);
+
+            if (board[i][j] == 0) {
+                if (liveNeighCells == 3) {
+                    tempBoard[i][j] = 1;
+                } else {
+                    tempBoard[i][j] = 0;
                 }
-            }
-
-            liveNeighCells -= board[i][j];
-
-            if ((board[i][j] == 1) && (liveNeighCells < 2 || liveNeighCells > 3)) {
-                tempBoard[i][j] = 0;
-            } else if ((board[i][j] == 0) && (liveNeighCells == 3)) {
-                tempBoard[i][j] = 1;
             } else {
-                tempBoard[i][j] = board[i][j];
+                if (liveNeighCells < 2 || liveNeighCells > 3) {
+                    tempBoard[i][j] = 0;
+                } else {
+                    tempBoard[i][j] = 1;
+                }
             }
         }
     }
@@ -77,5 +95,3 @@ void printToFile(int N, FILE *file) {
     }
     fprintf(file, "\n");
 }
-
-
